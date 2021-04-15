4ea015e6723c41133926b3cfa6c8b743dc52167e
Nikolay Trakiyski <ntrakiyski@gmail.com>
2021-04-15 19:27:28 +0300

    third  commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Gloria Rusenova Portfolio</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta property="og:title" content="Gloria Rusenova" />
+        <meta name="description" content="Gloria Rusenova Portfolio" />
+        <meta property="og:description" content="Gloria Rusenova Portfolio" />
+        <meta property="og:image" content="/media/thumbnail.jpg" />
+        <link rel="apple-touch-icon" href="/media/logo192.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,9 @@
-import Head from "next/head";
+
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Gloria Rusenova Portfolio</title>
-        <link rel="icon" href="/favicon.ico" />
-        <meta property="og:title" content="Gloria Rusenova" />
-        <meta name="description" content="Gloria Rusenova Portfolio" />
-        <meta property="og:description" content="Gloria Rusenova Portfolio" />
-        <meta property="og:image" content="/media/thumbnail.jpg" />
-        <link rel="apple-touch-icon" href="/media/logo192.png" />
-      </Head>
-
+    <div className={styles.container}> 
       <h1>Hi it`s me Gloria 22!</h1>
     </div>
   );
