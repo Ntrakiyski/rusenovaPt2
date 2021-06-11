4cadbfe6e4614d1fbc9d68fb8b7d65964546f5b1
Nikolay Trakiyski <ntrakiyski@gmail.com>
2021-06-11 15:02:57 +0300

    GA4

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
 import "../styles/globals.scss";
 import Head from "next/head";
 
+import * as gtag from "../lib/gtag";
+import { GA_TRACKING_ID } from "../lib/gtag";
+
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
   return (
     <>
       <Head>
@@ -12,6 +28,22 @@ function MyApp({ Component, pageProps }) {
         <meta property="og:description" content="Gloria Rusenova Portfolio" />
         <meta property="og:image" content="/media/thumbnail.jpg" />
         <link rel="apple-touch-icon" href="/media/logo192.png" />
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1.0"
--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,5 +1,6 @@
-import Document from "next/document";
+import Document, { Html, Head } from "next/document";
 import { ServerStyleSheet } from "styled-components";
+
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
